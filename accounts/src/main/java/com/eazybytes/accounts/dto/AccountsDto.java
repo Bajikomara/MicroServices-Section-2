912ecff1e82f56eb5c7020a0267b39799c803437
Bajikomara <kbaji52@gmail.com>
2025-07-01 01:09:55 +0530

    Created JPA entities and repositories for accounts and customers

---
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -0,0 +1,17 @@
+package com.eazybytes.accounts.dto;
+
+
+import lombok.Data;
+
+@Data
+public class AccountsDto {
+
+    private Long customerId;
+
+
+    private  Long accountNumber;
+
+    private String accountType;
+
+    private String branchAddress;
+}
