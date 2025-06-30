029f3fc76e086715693cb401a58c7f6b959b5b1a
Bajikomara <kbaji52@gmail.com>
2025-07-01 00:52:13 +0530

    Created JPA entities and repositories for accounts and customers

---
+++ accounts/src/main/java/com/eazybytes/accounts/Entity/Customer.java
@@ -0,0 +1,29 @@
+package com.eazybytes.accounts.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long customerId;
+
+    private  String name;
+
+    private String email;
+
+    private String mobileNumber;
+}
---
+++ accounts/src/main/java/com/eazybytes/accounts/Repository/AccountRepository.java
@@ -0,0 +1,9 @@
+package com.eazybytes.accounts.Repository;
+
+import com.eazybytes.accounts.Entity.Accounts;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Accounts, Long> {
+}
