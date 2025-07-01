9f7bc57d4de43b705ddd5f8826c47f9d3240eae3
Bajikomara <kbaji52@gmail.com>
2025-07-01 22:37:38 +0530

    Update API inside accounts microservice

--- accounts/src/main/java/com/eazybytes/accounts/Controller/AccountsController.java
+++ accounts/src/main/java/com/eazybytes/accounts/Controller/AccountsController.java
@@ -5,6 +5,7 @@ import com.eazybytes.accounts.Constants.AccountsConstants;
 import com.eazybytes.accounts.Service.IAccountsService;
 import com.eazybytes.accounts.dto.CustomerDto;
 import com.eazybytes.accounts.dto.ResponseDto;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -30,5 +31,19 @@ public class AccountsController {
     public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) { //
         CustomerDto customerDto = iAccountsService.fetchAccountDetails(mobileNumber);
         return ResponseEntity.status(HttpStatus.OK).body(customerDto);
+    }
+
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
+        boolean isUpdated = iAccountsService.updateAccount(customerDto);
+        if(isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(AccountsConstants.STATUS_200, AccountsConstants.MESSAGE_200));
+        }else{
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(AccountsConstants.STATUS_417, AccountsConstants.MESSAGE_417_UPDATE));
+        }
     }
 }
--- accounts/src/main/java/com/eazybytes/accounts/Service/IAccountsService.java
+++ accounts/src/main/java/com/eazybytes/accounts/Service/IAccountsService.java
@@ -10,5 +10,6 @@ public interface IAccountsService {
 
     CustomerDto fetchAccountDetails(String mobileNumber);
 
+    boolean updateAccount(CustomerDto customerDto);
 
 }
