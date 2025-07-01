e9f83b2c6c88204140d9c1ad3a17f1c8ff510358
Bajikomara <kbaji52@gmail.com>
2025-07-01 23:21:47 +0530

    Perform input data validations inside accounts microservice

--- accounts/src/main/java/com/eazybytes/accounts/Service/Impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/Service/Impl/AccountsServiceImpl.java
@@ -33,8 +33,6 @@ public class AccountsServiceImpl implements IAccountsService {
         if(optionalCustomer.isPresent()) {
             throw new CustomerAlreadyExistsException("Customer ALready Exists with give mobile number " + customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
 
@@ -48,8 +46,7 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccNumber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+
         return newAccount;
     }
 
--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,17 +1,20 @@
 package com.eazybytes.accounts.dto;
 
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
-    private Long customerId;
-
-
+    @NotEmpty(message = "Account number should not be null or empty")
+    @Pattern(regexp = "(^$|[0-9]{10})",message = "Mobile number must be 10 digits")
     private  Long accountNumber;
 
+    @NotEmpty(message = "Account type should not be null or empty")
     private String accountType;
 
+    @NotEmpty(message = "Branch address should not be null or empty")
     private String branchAddress;
 }
